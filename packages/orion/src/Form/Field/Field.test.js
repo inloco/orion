ff0164b53589c80f4d21233c3fce63fde7b49731
Marcel Rebouças <marcel.reboucas@inlocomedia.com>
2019-10-29 11:18:39 -0300

    fix(Form/Field): add the filled class if the input starts with a value

--- packages/orion/src/Form/Field/Field.test.js
+++ packages/orion/src/Form/Field/Field.test.js
@@ -6,46 +6,68 @@ import { Form, Input, Dropdown } from '../..'
 const label = 'Full name'
 const id = 'fullName'
 const placeholder = 'Enter your full name'
+const value = 'a name'
 
 const floatingLabelCssClass = 'floatingLabel'
+const filledCssClass = 'filled'
 
 const getFormField = container => container.querySelector('.field')
 
 describe('when the fields are passed as child', () => {
-  it('should have a floating label when the child is an Input and the size is default', () => {
-    const { container } = render(
-      <Form.Field>
-        <label>{label}</label>
-        <Input placeholder={placeholder} />
-      </Form.Field>
-    )
+  describe('when the child is an Input and the size is default', () => {
+    it('should have a floating label', () => {
+      const { container } = render(
+        <Form.Field>
+          <label>{label}</label>
+          <Input placeholder={placeholder} />
+        </Form.Field>
+      )
 
-    const formField = getFormField(container)
-    expect(formField).toHaveClass(floatingLabelCssClass)
+      const formField = getFormField(container)
+      expect(formField).toHaveClass(floatingLabelCssClass)
+      expect(formField.classList.contains(filledCssClass)).toBeFalsy()
+    })
+
+    it('should be filled if the Input has a value', () => {
+      const { container } = render(
+        <Form.Field>
+          <label>{label}</label>
+          <Input placeholder={placeholder} value={value} />
+        </Form.Field>
+      )
+
+      const formField = getFormField(container)
+      expect(formField).toHaveClass(floatingLabelCssClass)
+      expect(formField).toHaveClass(filledCssClass)
+    })
   })
 
-  it('should have a floating label when the child is a Dropdown and the size is default', () => {
-    const { container } = render(
-      <Form.Field>
-        <label>{label}</label>
-        <Dropdown placeholder={placeholder} />
-      </Form.Field>
-    )
+  describe('the child is a Dropdown and the size is default', () => {
+    it('should have a floating label', () => {
+      const { container } = render(
+        <Form.Field>
+          <label>{label}</label>
+          <Dropdown placeholder={placeholder} />
+        </Form.Field>
+      )
 
-    const formField = getFormField(container)
-    expect(formField).toHaveClass(floatingLabelCssClass)
+      const formField = getFormField(container)
+      expect(formField).toHaveClass(floatingLabelCssClass)
+    })
   })
 
-  it('should not have a floating label when the child is an Input and the size is small', () => {
-    const { container } = render(
-      <Form.Field>
-        <label>{label}</label>
-        <Input size="small" placeholder={placeholder} />
-      </Form.Field>
-    )
+  describe('when the child is an Input and the size is small', () => {
+    it('should not have a floating label', () => {
+      const { container } = render(
+        <Form.Field>
+          <label>{label}</label>
+          <Input size="small" placeholder={placeholder} />
+        </Form.Field>
+      )
 
-    const formField = getFormField(container)
-    expect(formField.classList.contains(floatingLabelCssClass)).toBeFalsy()
+      const formField = getFormField(container)
+      expect(formField.classList.contains(floatingLabelCssClass)).toBeFalsy()
+    })
   })
 
   it('should call onChange when the value changes', () => {
@@ -72,31 +94,48 @@ describe('when the fields are passed as child', () => {
 })
 
 describe('when the fields are passed as control', () => {
-  it('should have a floating label when the control is an Input and the size is default', () => {
-    const { container } = render(
-      <Form.Input label={label} placeholder={placeholder} />
-    )
+  describe('when the control is an Input and the size is default', () => {
+    it('should have a floating label', () => {
+      const { container } = render(
+        <Form.Input label={label} placeholder={placeholder} />
+      )
 
-    const formField = getFormField(container)
-    expect(formField).toHaveClass(floatingLabelCssClass)
+      const formField = getFormField(container)
+      expect(formField).toHaveClass(floatingLabelCssClass)
+      expect(formField.classList.contains(filledCssClass)).toBeFalsy()
+    })
+
+    it('should be filled if the Input has a value', () => {
+      const { container } = render(
+        <Form.Input label={label} placeholder={placeholder} value={value} />
+      )
+
+      const formField = getFormField(container)
+      expect(formField).toHaveClass(floatingLabelCssClass)
+      expect(formField).toHaveClass(filledCssClass)
+    })
   })
 
-  it('should have a floating label when the control is a Dropdown and the size is default', () => {
-    const { container } = render(
-      <Form.Dropdown label={label} placeholder={placeholder} />
-    )
+  describe('when the control is a Dropdown and the size is default', () => {
+    it('should have a floating label', () => {
+      const { container } = render(
+        <Form.Dropdown label={label} placeholder={placeholder} />
+      )
 
-    const formField = getFormField(container)
-    expect(formField).toHaveClass(floatingLabelCssClass)
+      const formField = getFormField(container)
+      expect(formField).toHaveClass(floatingLabelCssClass)
+    })
   })
 
-  it('should not have a floating label when the control is a Dropdown and the size is small', () => {
-    const { container } = render(
-      <Form.Dropdown label={label} size="small" placeholder={placeholder} />
-    )
+  describe('when the control is a Dropdown and the size is small', () => {
+    it('should not have a floating label', () => {
+      const { container } = render(
+        <Form.Dropdown label={label} size="small" placeholder={placeholder} />
+      )
 
-    const formField = getFormField(container)
-    expect(formField.classList.contains(floatingLabelCssClass)).toBeFalsy()
+      const formField = getFormField(container)
+      expect(formField.classList.contains(floatingLabelCssClass)).toBeFalsy()
+    })
   })
 
   it('should call onChange when the value changes', () => {
--- packages/orion/src/Form/Field/index.js
+++ packages/orion/src/Form/Field/index.js
@@ -1,7 +1,7 @@
 import _ from 'lodash'
 import cx from 'classnames'
 import PropTypes from 'prop-types'
-import React from 'react'
+import React, { useState } from 'react'
 import { Form } from '@inloco/semantic-ui-react'
 
 import Input from '../../Input'
@@ -15,28 +15,24 @@ const shouldHaveFloatingLabel = (field, size) =>
   (field === Input || field === Dropdown) && size === Sizes.DEFAULT
 
 const Field = ({ className, children, message, onChange, ...otherProps }) => {
-  const { size, control } = otherProps
-  const [controlFilled, setControlFilled] = React.useState(false)
+  const { size, control, value } = otherProps
+
+  let finalChildren = children
+  let finalOnChange = onChange
+  let floatingLabel = false
+  let warning = otherProps.warning
+  let initialFilled = false
 
   const handleChange = (e, data) => {
     setControlFilled(!!data.value)
     if (onChange) onChange(e, data)
   }
 
-  let finalChildren = children
-  let finalOnChange = onChange
-  let floatingLabel = false
-  let warning = otherProps.warning
-
-  if (shouldHaveFloatingLabel(control, size)) {
-    finalOnChange = handleChange
-    floatingLabel = true
-  }
-
   if (!_.isNil(children)) {
     finalChildren = React.Children.map(children, child => {
       if (shouldHaveFloatingLabel(child.type, child.props.size)) {
         floatingLabel = true
+        initialFilled = !_.isEmpty(child.props.value)
         warning = child.props.warning
         return React.cloneElement(child, {
           onChange: (e, data) => {
@@ -48,6 +44,14 @@ const Field = ({ className, children, message, onChange, ...otherProps }) => {
       return child
     })
   }
+
+  if (shouldHaveFloatingLabel(control, size)) {
+    finalOnChange = handleChange
+    floatingLabel = true
+    initialFilled = !_.isEmpty(value) || initialFilled
+  }
+
+  const [controlFilled, setControlFilled] = useState(initialFilled)
 
   const classes = cx(className, {
     filled: controlFilled,
