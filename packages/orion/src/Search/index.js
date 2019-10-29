409c07a8f497df8d45a02fc0e6e79199575e6335
Thalles Rodrigues <thalles.cezar@gmail.com>
2019-10-29 09:40:22 -0300

    refactor(Dropdown, Search): use loading string instead of object

--- packages/orion/src/Search/index.js
+++ packages/orion/src/Search/index.js
@@ -2,9 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Search as SemanticSearch } from '@inloco/semantic-ui-react'
 
-const LOADING_ICON = {
-  name: 'loading'
-}
+const LOADING_ICON = 'loading'
 
 const Search = ({ loading, icon, ...otherProps }) => {
   return <SemanticSearch {...otherProps} icon={loading ? LOADING_ICON : icon} />
