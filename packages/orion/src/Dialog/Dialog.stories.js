f42835231b0800c9310d8fd89c6be75658a8533d
Marcel Rebouças <marcel.reboucas@inlocomedia.com>
2019-11-05 11:17:10 -0300

    feature(Dialog): add the dialog component

---
+++ packages/orion/src/Dialog/Dialog.stories.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { text, boolean, withKnobs } from '@storybook/addon-knobs'
+
+import { Button, Dialog, Icon } from '../'
+
+export default {
+  title: 'Dialog',
+  decorators: [withKnobs]
+}
+
+export const standard = () => {
+  return (
+    <Dialog
+      trigger={<Button>Open Dialog</Button>}
+      warning={boolean('Warning', false)}
+      danger={boolean('Danger', false)}>
+      <Dialog.Header className="flex flex-col">
+        <Icon name="warning" className="mb-8" />
+        {text('Title', 'Dialog Title')}
+      </Dialog.Header>
+      <Dialog.Content>
+        {text(
+          'Content',
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+        )}
+      </Dialog.Content>
+      <Dialog.Actions>
+        <Button>Cancel</Button>
+        <Button primary>Continue</Button>
+      </Dialog.Actions>
+    </Dialog>
+  )
+}
