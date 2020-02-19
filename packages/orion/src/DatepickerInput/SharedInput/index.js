477b110cda21ad36950f732f90b776d9cbbac71e
Maira Bello <maira.bello@inloco.com.br>
2020-02-19 17:54:09 -0300

    fix(datepickerInput): Fix blinking when opening the popup with the calendars

--- packages/orion/src/DatepickerInput/SharedInput/index.js
+++ packages/orion/src/DatepickerInput/SharedInput/index.js
@@ -1,6 +1,6 @@
 import cx from 'classnames'
 import PropTypes from 'prop-types'
-import React from 'react'
+import React, { useState } from 'react'
 import { Popup } from '@inloco/semantic-ui-react'
 
 import Input from '../../Input'
@@ -13,15 +13,25 @@ const DatepickerSharedInput = ({
   value,
   ...otherProps
 }) => {
+  const [opening, setOpening] = useState()
+
+  // We add a special CSS class for a short time after opening the
+  // popup. That's because react dates takes a while to render, so
+  // we wait a little bit until it's done to avoid flickering.
+  const handleTriggerClick = () => {
+    setOpening(true)
+    setTimeout(() => setOpening(false))
+  }
   return (
     <Popup
-      className="datepicker-input-popup"
+      className={cx('datepicker-input-popup', { opening })}
       basic
       on="click"
       trigger={
         <div
           className={cx('datepicker-input-trigger', { disabled })}
-          data-testid="datepicker-trigger">
+          data-testid="datepicker-trigger"
+          onClick={handleTriggerClick}>
           <Input
             className={cx('datepicker-input', className)}
             disabled={disabled}
