5ecfdd460e15892a4eec66b2cba470c64b57a7da
Thalles Rodrigues <thalles.rodrigues@inloco.com.br>
2020-03-04 12:50:15 -0300

    feat(UserProfileButton): avoid extracting unnecessary props

--- packages/orion/src/UserProfile/UserProfileButton/index.js
+++ packages/orion/src/UserProfile/UserProfileButton/index.js
@@ -1,25 +1,9 @@
-import cx from 'classnames'
-import PropTypes from 'prop-types'
 import React from 'react'
 
 import Button from '../../Button'
 
-const UserProfileButton = ({ className, children, ...otherProps }) => {
-  return (
-    <Button
-      secondary
-      subtle
-      icon
-      className={cx('orion user-profile-button', className)}
-      {...otherProps}>
-      {children}
-    </Button>
-  )
-}
-
-UserProfileButton.propTypes = {
-  className: PropTypes.string,
-  children: PropTypes.node
+const UserProfileButton = props => {
+  return <Button secondary subtle icon {...props} />
 }
 
 export default UserProfileButton
