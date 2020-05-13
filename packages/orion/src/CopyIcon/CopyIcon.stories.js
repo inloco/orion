f2641df79df010c6f8822e73f42c1af49a86b535
llaryssa <llaryssa@me.com>
2020-05-13 17:04:15 -0300

    fix(CopyIcon.stories): fixes stories props

--- packages/orion/src/CopyIcon/CopyIcon.stories.js
+++ packages/orion/src/CopyIcon/CopyIcon.stories.js
@@ -15,8 +15,7 @@ export const basic = () => (
     value={text('Value', 'Drink water folks!')}
     content={text('Content', 'Copy a fun message')}
     successContent={text('Success Content', 'Copied!')}
-    tooltipPosition="bottom left"
+    tooltipPosition={text('Position', 'bottom left')}
     size={sizeKnob()}
-    className="text-icon-sm"
   />
 )
