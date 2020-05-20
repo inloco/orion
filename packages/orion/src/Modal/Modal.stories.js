cb31de348e9f4747f9eafe6f06c3c76b679c96f9
llaryssa <llaryssa@me.com>
2020-05-20 18:46:01 -0300

    feat(Modal.stories): adds long modal to stories

--- packages/orion/src/Modal/Modal.stories.js
+++ packages/orion/src/Modal/Modal.stories.js
@@ -25,3 +25,27 @@ export const standard = () => {
     </Modal>
   )
 }
+
+export const scrolling = () => {
+  return (
+    <Modal trigger={<Button>Open Long Modal</Button>} closeIcon>
+      <Modal.Header content={text('Title', 'Modal Title')} />
+      <Modal.Content>
+        <div className="my-64">A very long modal content</div>
+        <div className="my-64">A very loong modal content</div>
+        <div className="my-64">A very looong modal content</div>
+        <div className="my-64">A very loooong modal content</div>
+        <div className="my-64">A very looooong modal content</div>
+        <div className="my-64">A very loooooong modal content</div>
+        <div className="my-64">A very looooooong modal content</div>
+        <div className="my-64">A very loooooooong modal content</div>
+        <div className="my-64">A very looooooooong modal content</div>
+        <div className="my-64">A very loooooooooong modal content</div>
+      </Modal.Content>
+      <Modal.Actions>
+        <Button>Cancel</Button>
+        <Button primary>Yes</Button>
+      </Modal.Actions>
+    </Modal>
+  )
+}
