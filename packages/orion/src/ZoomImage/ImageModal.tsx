5b48d27261cac5bacaa3150769c01b57a468522a
Thalles Rodrigues <thalles.rodrigues@inloco.com.br>
2020-06-01 11:32:24 -0300

    feat(ZoomImage): create component ZoomImage

---
+++ packages/orion/src/ZoomImage/ImageModal.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import { Modal } from '@inloco/semantic-ui-react'
+
+import { Button } from '..'
+
+function ImageModal({ src, alt, onClose }: ImageModalProps) {
+  return (
+    <Modal open className="orion-zoom-image-modal">
+      <Button subtle secondary icon="close" onClick={onClose} />
+      <div className="image-wrapper">
+        <img src={src} alt={alt} />
+      </div>
+    </Modal>
+  )
+}
+
+type ImageModalProps = {
+  src: string
+  alt: string
+  onClose: () => void
+}
+
+export default ImageModal
