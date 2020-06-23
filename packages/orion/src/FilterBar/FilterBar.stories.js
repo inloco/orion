8e483ea4b5cbe6eccb40515994c1489654e2da31
Gileno Oliveira <gileno.oliveira@inloco.com.br>
2020-06-23 12:06:08 -0300

    feature(filter-bar): Make possible to FilterBar have another children types

--- packages/orion/src/FilterBar/FilterBar.stories.js
+++ packages/orion/src/FilterBar/FilterBar.stories.js
@@ -3,7 +3,7 @@ import { action } from '@storybook/addon-actions'
 import { withKnobs } from '@storybook/addon-knobs'
 import PropTypes from 'prop-types'
 
-import { FilterBar, Input } from '../'
+import { FilterBar, Input, Icon } from '../'
 
 const actions = {
   onChange: action('onChange')
@@ -22,6 +22,20 @@ export const basic = () => (
     <FilterBar.Filter text="Filter 2" name="filter2">
       {filterProps => <FilterStoryInput {...filterProps} />}
     </FilterBar.Filter>
+    <FilterBar.Filter text="Filter 3" name="filter3">
+      {filterProps => <FilterStoryInput {...filterProps} />}
+    </FilterBar.Filter>
+  </FilterBar>
+)
+
+export const differentFilterBarChildren = () => (
+  <FilterBar {...actions} className="flex items-center">
+    <Icon name="filter_list" className="mr-16" />
+    {true && (
+      <FilterBar.Filter text="Filter 1" name="filter1">
+        {filterProps => <FilterStoryInput {...filterProps} />}
+      </FilterBar.Filter>
+    )}
     <FilterBar.Filter text="Filter 3" name="filter3">
       {filterProps => <FilterStoryInput {...filterProps} />}
     </FilterBar.Filter>
