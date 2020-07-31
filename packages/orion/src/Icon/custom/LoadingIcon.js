fad2c86e1779bddcef002ba574449104c7ac5104
Marcel Rebouças <marcel.reboucas@inlocomedia.com>
2020-07-31 17:49:28 -0300

    refactor(LoadingIcon): add the children prop and ignore it

--- packages/orion/src/Icon/custom/LoadingIcon.js
+++ packages/orion/src/Icon/custom/LoadingIcon.js
@@ -2,12 +2,13 @@ import cx from 'classnames'
 import PropTypes from 'prop-types'
 import React from 'react'
 
-const LoadingIcon = ({ className, ...otherProps }) => (
-  <i className={cx('orion loading', className)} {...otherProps} children="" />
+const LoadingIcon = ({ className, children, ...otherProps }) => (
+  <i className={cx('orion loading', className)} {...otherProps} />
 )
 
 LoadingIcon.propTypes = {
-  className: PropTypes.string
+  className: PropTypes.string,
+  children: PropTypes.any
 }
 
 export default LoadingIcon
