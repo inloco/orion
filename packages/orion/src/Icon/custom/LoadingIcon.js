ea13b361bd337cc6197d30efa22ef0633ad28bdd
Marcel Rebouças <marcel.reboucas@inlocomedia.com>
2020-07-31 15:14:32 -0300

    fix(LoadingIcon): remove the children prop

--- packages/orion/src/Icon/custom/LoadingIcon.js
+++ packages/orion/src/Icon/custom/LoadingIcon.js
@@ -2,7 +2,7 @@ import cx from 'classnames'
 import PropTypes from 'prop-types'
 import React from 'react'
 
-const LoadingIcon = ({ className, children, ...otherProps }) => (
+const LoadingIcon = ({ className, ...otherProps }) => (
   <i className={cx('orion loading', className)} {...otherProps} />
 )
 
